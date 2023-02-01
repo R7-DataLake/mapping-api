e5624dfc512e8c8d7b2e080a9b88f1b7498ca7c2
Satit Rianpit <siteslave@users.noreply.github.com>
2023-02-01 09:43:37 +0700

    Update environments

--- src/app.ts
+++ src/app.ts
@@ -36,24 +36,24 @@ app.register(require('./plugins/db'), {
   options: {
     client: 'pg',
     connection: {
-      host: process.env.R7PLATFORM_LOGIN_DB_HOST || 'localhost',
-      user: process.env.R7PLATFORM_LOGIN_DB_USER || 'postgres',
-      port: Number(process.env.R7PLATFORM_LOGIN_DB_PORT) || 5432,
-      password: process.env.R7PLATFORM_LOGIN_DB_PASSWORD || '',
-      database: process.env.R7PLATFORM_LOGIN_DB_NAME || 'test',
+      host: process.env.R7PLATFORM_MAPPING_API_DB_HOST || 'localhost',
+      user: process.env.R7PLATFORM_MAPPING_API_DB_USER || 'postgres',
+      port: Number(process.env.R7PLATFORM_MAPPING_API_DB_PORT) || 5432,
+      password: process.env.R7PLATFORM_MAPPING_API_DB_PASSWORD || '',
+      database: process.env.R7PLATFORM_MAPPING_API_DB_NAME || 'test',
     },
-    searchPath: [process.env.R7PLATFORM_LOGIN_DB_SCHEMA || 'public'],
+    searchPath: [process.env.R7PLATFORM_MAPPING_API_DB_SCHEMA || 'public'],
     pool: {
       min: 10,
       max: 500
     },
-    debug: process.env.R7PLATFORM_LOGIN_DB_DEBUG === "Y" ? true : false,
+    debug: process.env.R7PLATFORM_MAPPING_API_DB_DEBUG === "Y" ? true : false,
   }
 })
 
 // JWT
 app.register(require('./plugins/jwt'), {
-  secret: process.env.R7PLATFORM_LOGIN_SECRET_KEY || '@1234567890@',
+  secret: process.env.R7PLATFORM_MAPPING_API_SECRET_KEY || '@1234567890@',
   sign: {
     iss: 'r7.moph.go.th',
     expiresIn: '10m'
@@ -70,7 +70,7 @@ app.register(require('./plugins/jwt'), {
 
 // hash password
 app.decorate('hashPassword', async (password: any) => {
-  const salt = process.env.R7PLATFORM_LOGIN_PASSWORD_SALT || 'gwuqpUkUm3jv07Ui0TCqZoZBuaJLztD9'
+  const salt = process.env.R7PLATFORM_MAPPING_API_PASSWORD_SALT || 'gwuqpUkUm3jv07Ui0TCqZoZBuaJLztD9'
   return await crypto
     .createHash('md5')
     .update(password + salt)
